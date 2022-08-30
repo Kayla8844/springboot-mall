d467b5b5d0287a96f35d64090d8092ebff8b0482
kayla <kayla@twjoin.com>
2022-08-30 15:22:30 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- src/main/java/com/kayla/springbootmall/dao/ProductDao.java
+++ src/main/java/com/kayla/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.kayla.springbootmall.dao;
 import com.kayla.springbootmall.dto.ProductRequest;
 import com.kayla.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/kayla/springbootmall/service/ProductService.java
+++ src/main/java/com/kayla/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.kayla.springbootmall.service;
 import com.kayla.springbootmall.dto.ProductRequest;
 import com.kayla.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
