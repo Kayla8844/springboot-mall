910a53427470c401fc4fd6053a627544de7e3ca0
kayla <kayla@twjoin.com>
2022-08-31 15:38:12 +0800

    商品功能 - 單元測試

--- src/main/java/com/kayla/springbootmall/model/Product.java
+++ src/main/java/com/kayla/springbootmall/model/Product.java
@@ -1,5 +1,6 @@
 package com.kayla.springbootmall.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kayla.springbootmall.constant.ProductCategory;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,13 +10,22 @@ import java.util.Date;
 @Getter
 @Setter
 public class Product {
+    @JsonProperty("productId")
     private Integer product_id;
+
+    @JsonProperty("productName")
     private String product_name;
     private ProductCategory category;
+
+    @JsonProperty("imageUrl")
     private String image_url;
     private Integer price;
     private Integer stock;
     private String description;
+
+    @JsonProperty("createdDate")
     private Date created_date;
+
+    @JsonProperty("lastModifiedDate")
     private Date last_modified_date;
 }
